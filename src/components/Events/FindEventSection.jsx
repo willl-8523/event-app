3db26003aee0236ee7d03503d598380fdfc7b400
willl85 <ngowilfried@gmail.com>
2023-09-21 14:38:35 +0200

    Add Dynamic Query Functions and Query Keys

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,10 +1,50 @@
-import { useRef } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useRef, useState } from 'react';
+import { fetchEvents } from '../../utils/http';
+import ErrorBlock from '../UI/ErrorBlock';
+import LoadingIndicator from '../UI/LoadingIndicator';
+import EventItem from './EventItem';
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', { search: searchTerm }],
+    queryFn: () => fetchEvents(searchTerm),
+  });
+  console.log(data, isPending, isError, error);
+
+  let content = <p>Please enter a search term and to find events.</p>;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occured"
+        message={error.info?.message || 'Failed to fetch search events'}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul>
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
   }
 
   return (
@@ -20,7 +60,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -5,23 +5,10 @@ import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import EventItem from './EventItem.jsx';
 
 export default function NewEventsSection() {
-  /**
-   * staleTime: 5000 => If I go to another page 5s before and come back
-   * It doesn't return the request
-   *
-   * gcTime: 30000 => The data will be stored in the cache for 30s,
-   * after which it will be deleted automatically.
-   *
-   * In DevTool, in the "No Throttling" tab, select "slow 3g" 
-   * to see the difference.
-   */
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['events'],
-    queryFn: fetchEvents,
-    staleTime: 5000,
+    queryFn: fetchEvents
   });
-
-  console.log(data, isPending, isError, error);
 
   let content;
 
