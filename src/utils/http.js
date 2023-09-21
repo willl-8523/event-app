346b2b8d3fa4659b851dfbe6d545ec5b7a94fe3a
willl85 <ngowilfried@gmail.com>
2023-09-21 18:44:26 +0200

    Add fetchEvent Function

--- src/utils/http.js
+++ src/utils/http.js
@@ -64,3 +64,21 @@ export async function fetchSelectableImages({ signal }) {
 
   return images;
 }
+
+export async function fetchEvent({ id, signal }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    signal,
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
