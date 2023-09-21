640179a2b99d8419095c380c54612a8b37a63f84
willl85 <ngowilfried@gmail.com>
2023-09-21 19:02:30 +0200

    Add deleteEvent Function

--- src/utils/http.js
+++ src/utils/http.js
@@ -82,3 +82,18 @@ export async function fetchEvent({ id, signal }) {
   return event;
 }
 
+export async function deleteEvent({ id }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
+}
+
