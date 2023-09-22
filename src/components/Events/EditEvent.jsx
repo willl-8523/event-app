2357285bac7d77ef55d89f2dcd6608922d16120e
willl85 <ngowilfried@gmail.com>
2023-09-22 12:50:18 +0200

    Change the Title to header element

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -2,8 +2,8 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { useQuery } from '@tanstack/react-query';
-import { fetchEvent } from '../../utils/http.js';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { fetchEvent, updateEvent } from '../../utils/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
@@ -16,7 +16,14 @@ export default function EditEvent() {
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   });
 
-  function handleSubmit(formData) {}
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate('../');
+  }
 
   function handleClose() {
     navigate('../');
@@ -43,7 +50,9 @@ export default function EditEvent() {
           }
         />
         <div className="form-actions">
-          <Link to='../' className='button'>Okay</Link>
+          <Link to="../" className="button">
+            Okay
+          </Link>
         </div>
       </>
     );
@@ -62,9 +71,5 @@ export default function EditEvent() {
     );
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      {content}
-    </Modal>
-  );
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
