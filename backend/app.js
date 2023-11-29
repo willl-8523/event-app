e6e3d42fb692709ff661977d3e035d8ef4ecac0b
willl85 <ngowilfried@gmail.com>
2023-11-29 16:43:32 +0100

    Add try catch in get /events

--- backend/app.js
+++ backend/app.js
@@ -27,31 +27,37 @@ app.get('/', (req, res) => {
 
 app.get('/events', async (req, res) => {
   const { max, search } = req.query;
-  const eventsFileContent = await fs.readFile('./data/events.json', 'utf8');
-  res.setHeader('Content-Type', 'application/json');
-  
-  let events = JSON.parse(eventsFileContent);
 
-  if (search) {
-    events = events.filter((event) => {
-      const searchableText = `${event.title} ${event.description} ${event.location}`;
-      return searchableText.toLowerCase().includes(search.toLowerCase());
+  try {
+    const eventsFileContent = await fs.readFile('./data/events.json', 'utf8');
+
+    let events = JSON.parse(eventsFileContent);
+
+    if (search) {
+      events = events.filter((event) => {
+        const searchableText = `${event.title} ${event.description} ${event.location}`;
+        return searchableText.toLowerCase().includes(search.toLowerCase());
+      });
+    }
+
+    if (max) {
+      events = events.slice(events.length - max, events.length);
+    }
+
+    res.json({
+      events: events.map((event) => ({
+        id: event.id,
+        title: event.title,
+        image: event.image,
+        date: event.date,
+        location: event.location,
+      })),
     });
+    
+  } catch (error) {
+    console.error(`Erreur lors de la lecture du fichier : ${error.message}`);
+    res.status(500).send('Erreur interne du serveur');
   }
-
-  if (max) {
-    events = events.slice(events.length - max, events.length);
-  }
-
-  res.json({
-    events: events.map((event) => ({
-      id: event.id,
-      title: event.title,
-      image: event.image,
-      date: event.date,
-      location: event.location,
-    })),
-  });
 });
 
 app.get('/events/images', async (req, res) => {
