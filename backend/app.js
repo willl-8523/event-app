8954f8ba6989a29bfeea1f942ef6483dc5dabc67
willl85 <ngowilfried@gmail.com>
2023-11-30 19:27:18 +0100

    Test response to /events

--- backend/app.js
+++ backend/app.js
@@ -27,41 +27,42 @@ app.get('/', (req, res) => {
 });
 
 app.get('/events', async (req, res) => {
-  const { max, search } = req.query;
+  res.json('Hello I am /events');
+  // const { max, search } = req.query;
 
-  try {
-    // const filepath = path.join('data', 'events.json');
-    const eventsFileContent = await fs.readFile('./backend/data/events.json', {
-      encoding: 'utf8',
-    });
+  // try {
+  //   // const filepath = path.join('data', 'events.json');
+  //   const eventsFileContent = await fs.readFile('./backend/data/events.json', {
+  //     encoding: 'utf8',
+  //   });
 
-    let events = JSON.parse(eventsFileContent);
+  //   let events = JSON.parse(eventsFileContent);
 
-    if (search) {
-      events = events.filter((event) => {
-        const searchableText = `${event.title} ${event.description} ${event.location}`;
-        return searchableText.toLowerCase().includes(search.toLowerCase());
-      });
-    }
+  //   if (search) {
+  //     events = events.filter((event) => {
+  //       const searchableText = `${event.title} ${event.description} ${event.location}`;
+  //       return searchableText.toLowerCase().includes(search.toLowerCase());
+  //     });
+  //   }
 
-    if (max) {
-      events = events.slice(events.length - max, events.length);
-    }
+  //   if (max) {
+  //     events = events.slice(events.length - max, events.length);
+  //   }
 
-    res.json({
-      events: events.map((event) => ({
-        id: event.id,
-        title: event.title,
-        image: event.image,
-        date: event.date,
-        location: event.location,
-      })),
-    });
+  //   res.json({
+  //     events: events.map((event) => ({
+  //       id: event.id,
+  //       title: event.title,
+  //       image: event.image,
+  //       date: event.date,
+  //       location: event.location,
+  //     })),
+  //   });
     
-  } catch (error) {
-    console.error(`Erreur lors de la lecture du fichier : ${error.message}`);
-    res.status(500).send('Erreur interne du serveur');
-  }
+  // } catch (error) {
+  //   console.error(`Erreur lors de la lecture du fichier : ${error.message}`);
+  //   res.status(500).send('Erreur interne du serveur');
+  // }
 });
 
 app.get('/events/images', async (req, res) => {
