b0b456d75f4901d12fea9291df40b9a2e8252825
willl85 <ngowilfried@gmail.com>
2023-11-30 16:02:27 +0100

    Change localhost link

--- frontend/src/utils/http.js
+++ frontend/src/utils/http.js
@@ -7,7 +7,7 @@ export async function fetchEvents({ signal, searchTerm, max }) {
    * Signal is to set the default parameter sent by usequery
    */
 
-  let url = 'http://localhost:3000/events';
+  let url = 'https://event-app-api.vercel.app/';
 
   if (searchTerm && max) {
     url += '?search=' + searchTerm + '&max=' + max;
