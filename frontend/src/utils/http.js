6fb1cb928ff436e43f79364fbb1eb875572c4af9
willl85 <ngowilfried@gmail.com>
2023-12-01 13:21:24 +0100

    Change link to fetchSelectableImages

--- frontend/src/utils/http.js
+++ frontend/src/utils/http.js
@@ -53,9 +53,12 @@ export async function createNewEvent(eventData) {
 }
 
 export async function fetchSelectableImages({ signal }) {
-  const response = await fetch(`http://localhost:3000/events/images`, {
-    signal,
-  });
+  const response = await fetch(
+    `https://event-app-api.vercel.app/events/images`,
+    {
+      signal,
+    }
+  );
 
   if (!response.ok) {
     const error = new Error('An error occurred while fetching the images');
