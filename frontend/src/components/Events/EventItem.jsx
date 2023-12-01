dffd95eb27565da7709a50e9efcaf320268a80f1
willl85 <ngowilfried@gmail.com>
2023-12-01 13:44:49 +0100

    Change link EventItem components

--- frontend/src/components/Events/EventItem.jsx
+++ frontend/src/components/Events/EventItem.jsx
@@ -8,10 +8,7 @@ export default function EventItem({ event }) {
   });
   return (
     <article className="event-item">
-      <img
-        src={`https://event-app-api.vercel.app/${event.image}`}
-        alt={event.title}
-      />
+      <img src={`https://event-app-api.vercel.app/${event.image}`} alt={event.title} />
       <div className="event-item-content">
         <div>
           <h2>{event.title}</h2>
